345ed38e2886386de2d0762eef8e0726652e8a39
javierMx13 <javierxup@gmail.com>
2020-06-17 23:08:20 -0700

    Hasta js assigment

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var x;
+x=5;
+var x1,x2;
+x1="hello";
+x2="perro";
+var x3=x1+x2;
+//this comentario es ejemplo inutil owo
+var y = 10;
+y *= 5;
+function myFunction() {
+    document.getElementById("dropdown-we").innerHTML = window.alert("so sad");
+}
