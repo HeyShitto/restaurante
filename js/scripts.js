45fdf00f81f9363edc9f2055418b4353d53a93b6
javierMx13 <javierxup@gmail.com>
2020-06-19 03:49:41 -0700

    Hasta js Comparison

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,37 @@ var sum=suma(1,2);
 var car = ["Fiat","500", "white"];
 car.pop();
 car.push("green");
+car.sort();
+car.reverse();
+
+var tx = "";
+var numbers = [45, 4, 9, 16, 25];
+numbers.forEach(myFunction);
+var numbers1 = [45, 4, 9, 16, 25];
+var numbers2 = numbers1.map(myFunction);
+function forcito(value, index, array) {
+    tx = tx + value + "<br>";
+}
+var numbers = [45, 4, 9, 16, 25];
+var over18 = numbers.filter(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+var d = new Date("October 13, 2014 11:13:00");
+document.getElementById("demo").innerHTML = d;
+var d = new Date();
+document.getElementById("demo").innerHTML = d.getFullYear();
+var d = new Date();
+d.setFullYear(2020);
+document.getElementById("demo").innerHTML = d;
+
+var pi =Math.PI; 
+pi = Math.round(pi);
+var raiz =Math.sqrt(64);
+var absolu=Math.abs(-4.7);
+var rand1 =Math.floor(Math.random() * 10);
+
 
 function myFunction() {
     document.getElementById("dropdown-we").innerHTML = window.alert("so sad");
