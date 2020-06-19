864964c2d6c1185cb7611a16b45071539a432c03
javierMx13 <javierxup@gmail.com>
2020-06-18 18:02:03 -0700

    Hasta js arrays methods

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,36 @@ var x1,x2;
 x1="hello";
 x2="perro";
 var x3=x1+x2;
+x.toExponential(2);
+x.toFixed(2);
 //this comentario es ejemplo inutil owo
 var y = 10;
+var dec=1.3;
+var nada="";
+var nulo=null;
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length;
+var pos = str.search("Y");
+
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue";
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+person = undefined;
 y *= 5;
+
+var sum=suma(1,2);
+var car = ["Fiat","500", "white"];
+car.pop();
+car.push("green");
+
 function myFunction() {
     document.getElementById("dropdown-we").innerHTML = window.alert("so sad");
+}
+function suma(n1,n2){
+    return n1+n2;
+}
+function showme(){
+    document.getElementById("dropdown-we").innerHTML = person["firstName"] + " " + person["lastName"];
 }
